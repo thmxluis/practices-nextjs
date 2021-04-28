862a26c846652a3c537b48acb8e37974043e0d6c
Luis Gonzalez <luis.gonzalez@orbitec.pe>
2021-04-28 10:45:36 -0500

    first commiit

---
+++ firebase/client.js
@@ -0,0 +1,36 @@
+import * as firebase from "firebase";
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBtJ4XOo4LVbgPsM9tq_E9sdeiYcRvgVw0",
+  authDomain: "devter-71e62.firebaseapp.com",
+  projectId: "devter-71e62",
+  storageBucket: "devter-71e62.appspot.com",
+  messagingSenderId: "320170789419",
+  appId: "1:320170789419:web:43b9384e2e589461b5f86e",
+  measurementId: "G-Z3TJGLFJMQ",
+};
+
+!firebase.apps.length && firebase.initializeApp(firebaseConfig);
+
+const mapUserFromFirebaseAuthToUser = (user) => {
+  const { displayName, email, photoURL } = user;
+
+  return {
+    avatar: photoURL,
+    username: displayName,
+    email,
+  };
+};
+
+export const onAuthStateChanged = (onChange) => {
+  return firebase.auth().onAuthStateChanged((user) => {
+    const normalizedUser = mapUserFromFirebaseAuthToUser(user);
+    onChange(normalizedUser);
+  });
+};
+
+export const loginWithGitHub = () => {
+  const githubProvider = new firebase.auth.GithubAuthProvider();
+  return firebase.auth().signInWithPopup(githubProvider);
+};
---
+++ pages/index.js
@@ -0,0 +1,89 @@
+import { useEffect, useState } from "react";
+import Head from "next/head";
+import AppLayout from "../components/AppLayout";
+import { colors } from "../styles/theme";
+import Button from "../components/Button";
+import GitHub from "../components/Icons/GitHub";
+
+import { loginWithGitHub, onAuthStateChanged } from "../firebase/client";
+
+export default function Home() {
+  const [user, setUser] = useState(undefined);
+  // console.log(user);
+  useEffect(() => {
+    onAuthStateChanged(setUser);
+  }, []);
+
+  const handleClick = () => {
+    loginWithGitHub()
+      .then(setUser)
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  return (
+    <>
+      <Head>
+        <title>devter 🐦</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <AppLayout>
+        <section>
+          <img src="/devter-logo.png" alt="Logo" />
+          <h1>Devter</h1>
+          <h2>
+            Talk about development
+            <br />
+            with developers 👩‍💻👨‍💻
+          </h2>
+
+          <div>
+            {user === undefined && (
+              <Button onClick={handleClick}>
+                <GitHub fill="#fff" width={24} height={24} />
+                Login with GitHub
+              </Button>
+            )}
+            {user && user.avatar && (
+              <div>
+                <img src={user.avatar} />
+                <strong>{user.username}</strong>
+              </div>
+            )}
+          </div>
+        </section>
+      </AppLayout>
+
+      <style jsx>{`
+        img {
+          width: 120px;
+        }
+
+        div {
+          margin-top: 16px;
+        }
+
+        section {
+          display: grid;
+          height: 100%;
+          place-content: center;
+          place-items: center;
+        }
+
+        h1 {
+          color: ${colors.secondary};
+          font-weight: 800;
+          margin-bottom: 16px;
+        }
+
+        h2 {
+          color: ${colors.primary};
+          font-size: 21px;
+          margin: 0;
+        }
+      `}</style>
+    </>
+  );
+}
